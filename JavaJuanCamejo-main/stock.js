117bbb40e771e01f8ff92de98dfcef854fe0df2c
Juan Camejo <juancamejo@hotmail.com>
2022-10-15 04:27:06 -0300

    Fetch1

--- JavaJuanCamejo-main/stock.js
+++
@@ -1,6 +0,0 @@
-let stockProductos = [
-    {id: 1, nombre: "Buzo", tipo: "buzo", cantidad: 1, desc: "Un buzo a tu estilo", precio: 3000, talle: "L", img: './Imagenes/buzo.jpg'},
-    {id: 2, nombre: "Remera", tipo: "remera", cantidad: 1, desc: "Una remera con tu estilo", precio: 1500, talle: "L", img: './Imagenes/remera.webp'},
-    {id: 3, nombre: "Jean", tipo: "jean", cantidad: 1, desc: "Un buzo que re va con vos", precio: 5000, talle: "M", img: './Imagenes/jean.jpg'},
-    {id: 4, nombre: "Zapatillas", tipo: "Zapatillas", cantidad: 1, desc: "Te acompaña a todos lados", precio: 6000, talle: "S", img: './Imagenes/calzadodeportivo.jpg'},
-]
